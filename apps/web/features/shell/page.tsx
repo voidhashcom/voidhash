112d2591cff948570d475501d61cc1c1fa659687
Dominik Vít <slipiklp@gmail.com>
2025-04-18 13:51:32 +0200

    fix: breadcrumbs added more space than needed

--- apps/web/features/shell/page.tsx
+++ apps/web/features/shell/page.tsx
@@ -24,44 +24,42 @@ export function Page({
 }) {
 	return (
 		<>
-			<header className="flex h-16 shrink-0 items-center gap-2">
-				<div className="flex items-center gap-2 px-4">
-					{breadcrumbs && (
-						<>
-							<Separator orientation="vertical" className="mr-2 h-4" />
-							<Breadcrumb>
-								<BreadcrumbList>
-									{breadcrumbs?.map((breadcrumb, index) => (
-										<Fragment key={breadcrumb.url ?? breadcrumb.title}>
-											{index < breadcrumbs.length - 1 && (
-												<BreadcrumbItem className="hidden md:block">
-													<BreadcrumbLink href={breadcrumb.url}>
-														{breadcrumb.title}
-													</BreadcrumbLink>
-												</BreadcrumbItem>
-											)}
-											{index === breadcrumbs.length - 1 && (
-												<BreadcrumbItem>
-													<BreadcrumbPage>
-														{breadcrumb.isLoading ? (
-															<Skeleton className="w-20 h-4" />
-														) : (
-															breadcrumb.title
-														)}
-													</BreadcrumbPage>
-												</BreadcrumbItem>
-											)}
-											{index !== breadcrumbs.length - 1 && (
-												<BreadcrumbSeparator className="hidden md:block" />
-											)}
-										</Fragment>
-									))}
-								</BreadcrumbList>
-							</Breadcrumb>
-						</>
-					)}
-				</div>
-			</header>
+			{breadcrumbs && breadcrumbs.length > 0 && (
+				<header className="flex h-16 shrink-0 items-center gap-2">
+					<div className="flex items-center gap-2 px-4">
+						<Separator orientation="vertical" className="mr-2 h-4" />
+						<Breadcrumb>
+							<BreadcrumbList>
+								{breadcrumbs?.map((breadcrumb, index) => (
+									<Fragment key={breadcrumb.url ?? breadcrumb.title}>
+										{index < breadcrumbs.length - 1 && (
+											<BreadcrumbItem className="hidden md:block">
+												<BreadcrumbLink href={breadcrumb.url}>
+													{breadcrumb.title}
+												</BreadcrumbLink>
+											</BreadcrumbItem>
+										)}
+										{index === breadcrumbs.length - 1 && (
+											<BreadcrumbItem>
+												<BreadcrumbPage>
+													{breadcrumb.isLoading ? (
+														<Skeleton className="w-20 h-4" />
+													) : (
+														breadcrumb.title
+													)}
+												</BreadcrumbPage>
+											</BreadcrumbItem>
+										)}
+										{index !== breadcrumbs.length - 1 && (
+											<BreadcrumbSeparator className="hidden md:block" />
+										)}
+									</Fragment>
+								))}
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</header>
+			)}
 			<div className={cn("p-4 py-8", className)}>{children}</div>
 		</>
 	);
