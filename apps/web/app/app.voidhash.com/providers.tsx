9dd2d6cf4ddcfebadf980e222b3e78269e541687
Dominik Vít <slipiklp@gmail.com>
2025-05-02 10:49:51 +0200

    feat: progress bar + loading spinners

--- apps/web/app/app.voidhash.com/providers.tsx
+++ apps/web/app/app.voidhash.com/providers.tsx
@@ -1,11 +1,18 @@
+"use client";
 import { TRPCReactProvider } from "@/features/trpc/react";
 import { Toaster } from "@voidhash/ui";
-
+import { Next13ProgressBar } from "next13-progressbar";
 export function AppProviders({ children }: { children: React.ReactNode }) {
 	return (
 		<TRPCReactProvider>
 			{children}
 			<Toaster />
+			<Next13ProgressBar
+				height="2px"
+				color="#005EFF"
+				options={{ showSpinner: false }}
+				showOnShallow
+			/>
 		</TRPCReactProvider>
 	);
 }
