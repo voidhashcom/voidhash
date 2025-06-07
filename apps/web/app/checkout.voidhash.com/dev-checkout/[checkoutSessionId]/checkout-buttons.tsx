676f9b85dab7e2f0a934491fdf925db33b4d0d5f
Dominik Vít <slipiklp@gmail.com>
2025-06-07 22:52:47 +0200

    fix: dev checkout redirection logic move redirection browser

---
+++ apps/web/app/checkout.voidhash.com/dev-checkout/[checkoutSessionId]/checkout-buttons.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+	cancelDevCheckoutPurchaseAction,
+	confirmDevCheckoutPurchaseAction,
+} from "@/lib/nextjs/server-actions";
+import { Button } from "@voidhash/ui";
+import { useAction } from "next-safe-action/hooks";
+import { redirect } from "next/navigation";
+
+export function CheckoutButtons({
+	checkoutSessionId,
+}: {
+	checkoutSessionId: string;
+}) {
+	const { execute: handleConfirm } = useAction(
+		confirmDevCheckoutPurchaseAction,
+		{
+			onSuccess: (data) => {
+				console.log(data);
+				redirect(
+					`${data?.data ?? ""}?checkoutSessionId=${checkoutSessionId}&success=true`
+				);
+			},
+			onError: (error) => {
+				console.log(error);
+			},
+		}
+	);
+
+	const { execute: handleCancel } = useAction(cancelDevCheckoutPurchaseAction, {
+		onSuccess: (data) => {
+			console.log(data);
+			redirect(
+				`${data?.data ?? ""}?checkoutSessionId=${checkoutSessionId}&error=cancelled`
+			);
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
+
+	return (
+		<>
+			<Button
+				onClick={() => handleConfirm({ checkoutSessionId })}
+				className="w-full"
+			>
+				Confirm Purchase
+			</Button>
+
+			<Button
+				onClick={() => handleCancel({ checkoutSessionId })}
+				variant="outline"
+				className="w-full"
+			>
+				Cancel
+			</Button>
+		</>
+	);
+}
