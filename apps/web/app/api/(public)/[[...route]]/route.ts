11128b1f768a14cc32c054981588c3065f8dcf80
Dominik Vít <slipiklp@gmail.com>
2025-07-10 17:42:51 +0200

    fix: move to zod 4

--- apps/web/app/api/(public)/[[...route]]/route.ts
+++ apps/web/app/api/(public)/[[...route]]/route.ts
@@ -1,9 +1,9 @@
-// import { app } from "@/lib/api/api";
-// import { handle } from "hono/vercel";
+import { app } from "@/lib/api/api";
+import { handle } from "hono/vercel";
 
-// export const runtime = "nodejs";
+export const runtime = "nodejs";
 
-// export const GET = handle(app);
-// export const POST = handle(app);
-// export const PUT = handle(app);
-// export const DELETE = handle(app);
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
