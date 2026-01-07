6bffac67fead7fbcb75b1219ace7f1cf8e88dd88
Dominik Vít <slipiklp@gmail.com>
2026-01-07 21:38:11 +0100

    wip

---
+++ examples/react-native-example/utils/voidhash/_schema.ts
@@ -0,0 +1,44 @@
+import { schemaConfiguration, unlockablePerk } from "@voidhash/react-native";
+
+export const sc = schemaConfiguration({
+	perks: {
+		allAccess: unlockablePerk("all-access", {
+			name: "All Access",
+		}),
+	},
+	providers: {
+		// appleAppStore: true,
+		// googlePlay: true,
+	},
+});
+
+export const monthlySub = sc.subscription("monthly_sub", {
+	name: "Monthly",
+	perks: {
+		allAccess: true,
+	},
+	providers: {
+		// appleAppStore: {
+		//   productId: "test_group_monthly",
+		// },
+		// googlePlay: {
+		//   productId: "com.voidhash.example.monthly",
+		// },
+	},
+});
+
+export const yearlySub = sc.subscription("yearly_sub", {
+	name: "Yearly",
+	perks: {
+		allAccess: true,
+	},
+	providers: {
+		// appleAppStore: {
+		//   productId: "test_group_yearly",
+		// },
+		// googlePlay: {
+		//   basePlanId: "com.voidhash.example.yearly.base",
+		//   productId: "com.voidhash.example.yearly",
+		// },
+	},
+});
--- examples/react-native-example/voidhash.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "voidhash-cli";
-
-export default defineConfig({
-  project: "dev",
-  schema: "utils/voidhash/schema.ts",
-  team: "voidhash-sro",
-});
